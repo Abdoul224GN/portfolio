6d53a3960579a4457642ff2e60a718a4ebcbc416
Abdoul Karim <karimdiallo625@gmail.com>
2025-04-18 17:25:04 +0000

    Changement de la police et suppression du code et classe CSS inutile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,25 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type {Metadata} from "next";
+import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
-  title: "Abdoul Karim Diallo",
-  description: "Portfolio de Abdoul Karim Diallo développeur web passionné spécialisé en développement front-end et back end. Découvrez mes projet, compétences et experiences.",
+    title: "Abdoul Karim Diallo",
+    description: "Portfolio de Abdoul Karim Diallo développeur web passionné spécialisé en développement front-end et back end. Découvrez mes projet, compétences et experiences.",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    return (
+        <html lang="en">
+        <body
+
+        >
         {children}
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
