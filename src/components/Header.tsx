b64305b3bc50f156da9ee18cef3ddff829bd3db5
Abdoul Karim <karimdiallo625@gmail.com>
2025-04-20 09:35:26 +0000

    feat: Création des liens de navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,13 @@
 export default function Header() {
-    return <p>Header</p>
+    return <>
+        <header className="w-full flex justify-center space-between">
+            <div className="border-border border-2 bg-muted w-1/2 rounded-full pl-6 pr-6 flex justify-center gap-10">
+                <a href="/" className="text-muted-foreground">Home</a>
+                <a href="/">Service</a>
+                <a href="/">Resume</a>
+                <a href="/">Work</a>
+                <a href="/">Contact</a>
+            </div>
+        </header>
+    </>
 }
