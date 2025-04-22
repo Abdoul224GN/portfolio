05de8c00c0c09869074c816f19c34cb3c5203111
Abdoul Karim <karimdiallo625@gmail.com>
2025-04-22 17:34:50 +0000

    feat: Ajout des icônes et finalisation du "Header"

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,20 @@
+"use client"
+import {GithubIcon, LinkDin, TwitterIcon} from "../../public/Image"
+import Nav from "@/components/Nav";
+
 export default function Header() {
     return <>
-        <header className="w-full flex justify-center space-between">
-            <div className="border-border border-2 bg-muted w-1/2 rounded-full pl-6 pr-6 flex justify-center gap-10">
-                <a href="/" className="text-muted-foreground">Home</a>
-                <a href="/">Service</a>
-                <a href="/">Resume</a>
-                <a href="/">Work</a>
-                <a href="/">Contact</a>
+        <header className="h-8  flex justify-between items-center w-full">
+            <p className=" flex-1/3 text-2xl">Abdoul<span className="text-accent">.</span></p>
+            <Nav/>
+            <div
+                className="h-full w-[125px] flex items-center justify-end pl-0.5 pr-0.5 pt-0 flex-1/3  ">
+                <div
+                    className="rounded-full border-border border-2 bg-muted h-full w-[125px] flex justify-between items-center pl-0.5 pr-0.5 pt-0 ">
+                    <GithubIcon size={9}/>
+                    <LinkDin size={9}/>
+                    <TwitterIcon size={30}/>
+                </div>
             </div>
         </header>
     </>
