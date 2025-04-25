4e3340b03e8736f4d4a1ba8cf90748a6eed012f8
Abdoul Karim <karimdiallo625@gmail.com>
2025-04-25 23:50:49 +0000

    feat: Développement de la partie description

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,3 +1,31 @@
-export default function Home(){
-    return <p>Home Page</p>
+import Header from "@/components/Header";
+import {Button} from "@/components/ui/button";
+import {FacebookIcon, GithubIcon, InstagramIcon, LinkDin, TwitterIcon} from "../../../public/Image";
+import Link from "@/components/ui/Link";
+
+export default function Home() {
+    return <>
+        <Header/>
+        <section className="h-full flex items-center p-25">
+            <div className="w-3/5 h-1/2">
+                <p>FullStack Software Developper</p>
+                <h1 className="text-7xl font-semibold text-foreground">Hello I'm<br/> <span className="text-accent">Abdoul Diallo</span>
+                </h1>
+                <p>
+                    Passionate full-stack developer. I build performance and intuitive
+                    application using framework like Spring Boot and NextJS. My
+                    focus is on code quality and optimal user experience. Each
+                    project reflects my commitment to delivering innovative solution.
+                </p>
+                <div className={`pt-5 flex justify-start gap-10 `}>
+                    <Button>Download CV</Button>
+                    <Link><GithubIcon/></Link>
+                    <Link><LinkDin/></Link>
+                    <Link><TwitterIcon/></Link>
+                    <Link><FacebookIcon/></Link>
+                    <Link><InstagramIcon/></Link>
+                </div>
+            </div>
+        </section>
+    </>
 }
