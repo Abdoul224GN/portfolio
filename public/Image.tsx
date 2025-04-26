785164fad76b842d71b6bc6ff6020abc0f8302a9
Abdoul Karim <karimdiallo625@gmail.com>
2025-04-26 14:22:40 +0000

    feat: Ajout de la photo de l'avatar et l'animation

--- public/Image.tsx
+++ public/Image.tsx
@@ -17,3 +17,20 @@ export function TwitterIcon({className}:{className?:string}) {
         <img src="images/twitter.png" className={`h-full ${className}`} alt="twitter" />
     )
 }
+
+export function FacebookIcon({className}:{className?:string}) {
+    return (
+        <img src="images/Facebook.png" className={`h-full ${className}` } alt="facebook" />
+    )
+}
+
+export function InstagramIcon({className}:{className?:string}) {
+    return (
+        <img src="images/Instagram.png" className={`h-full ${className}`} alt="instagram" />
+    )
+}
+export function AvatarImg({className}:{className?:string}) {
+    return (
+        <img src="images/Avatar.png" className={`h-full ${className}`} alt="avatar" />
+    )
+}
---
+++ src/app/home/Avatar.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import {motion} from "framer-motion"
+import {AvatarImg} from "../../../public/Image";
+
+export default function Avatar() {
+    return <>
+        <div className="h-full w-full relative">
+            <motion.div className="flex items-center justify-center">
+                <div className={`absolute`}>
+                    <AvatarImg className={`rounded-full size-[300px] object-contain order-2 `}/>
+                </div>
+                <motion.svg className={`h-[350px] w-[350px]`} fill="transparent" xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 506 506">
+                    <motion.circle
+                        className={`order1`}
+                        cx="250"
+                        cy="250"
+                        r="240"
+                        stroke="#FFCDAC"
+                        strokeWidth="6"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        initial={{strokeDasharray:"24 10 0 0"}}
+                        animate={{
+                            strokeDasharray:["15 120 25 25", "16 25 92 72","4 250 22 22"],
+                            rotate:[120, 360],
+                        }}
+                        transition={{
+                            duration:20,
+                            repeat: Infinity,
+                            repeatType:"reverse",
+                        }}
+                    />
+                </motion.svg>
+            </motion.div>
+
+        </div>
+    </>
+}
