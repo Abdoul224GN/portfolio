c60f35f1c4db83981a823e953d4d86f86b4dcdb2
Abdoul Karim <karimdiallo625@gmail.com>
2025-04-27 15:57:51 +0000

    feat: Mise en place de responsivite du corps de la page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,39 +1,44 @@
-import Header from "@/components/Header";
 import {Button} from "@/components/ui/button";
 import {FacebookIcon, GithubIcon, InstagramIcon, LinkDin, TwitterIcon} from "../../../public/Image";
 import Link from "@/components/ui/Link";
 import Avatar from "./Avatar";
-import Stats from "@/app/home/Stats";
 
 export default function Home() {
     return <>
-        <Header/>
-        <section className={`pr-30 pl-30 flex flex-col justify-items-center pt-15 pb-15 items-center h-full`}>
-            <section className=" flex justify-between0">
-                <div className="w-3/5 h-1/2">
-                    <p>FullStack Software Developper</p>
-                    <h1 className="text-7xl font-semibold text-foreground">Hello I'm<br/> <span className="text-accent">Abdoul Diallo</span>
+        {/*<Header/>*/
+        }
+        <section className={` flex flex-col justify-items-center h-full md:px-5 lg:px-30`}>
+
+            <div className={`flex flex-col md:flex-row items-center`}>
+                <div className="order-2 md:w-3/5 flex flex-col md:order-none pt-10">
+                    <p className={`text-center md:text-left`}>FullStack Software Developper</p>
+                    <h1 className="md:text-7xl md:text-left  text-4xl  text-center font-semibold text-foreground">Hello
+                        I'm<br/> <span
+                            className="text-accent">Abdoul Diallo</span>
                     </h1>
-                    <p>
+                    <p className={`text-center md:text-left`}>
                         Passionate full-stack developer. I build performance and intuitive
                         application using framework like Spring Boot and NextJS. My
                         focus is on code quality and optimal user experience. Each
                         project reflects my commitment to delivering innovative solution.
                     </p>
-                    <div className={`pt-5 flex justify-start gap-10 `}>
-                        <Button>Download CV</Button>
-                        <Link><GithubIcon/></Link>
-                        <Link><LinkDin/></Link>
-                        <Link><TwitterIcon/></Link>
-                        <Link><FacebookIcon/></Link>
-                        <Link><InstagramIcon/></Link>
+                    <div className={`flex flex-col md:flex-row items-center justify-items-center pt-5 pb-5 gap-4`}>
+                        <div className={`h-full flex items-center`}>
+                            <Button>Download CV</Button>
+                        </div>
+                        <div className={`flex justify-between items-center gap-4 h-full`}>
+                            <Link><GithubIcon/></Link>
+                            <Link><LinkDin/></Link>
+                            <Link><TwitterIcon/></Link>
+                            <Link><FacebookIcon/></Link>
+                            <Link><InstagramIcon/></Link>
+                        </div>
                     </div>
                 </div>
-                <div className={`flex items-end justify-end`}><Avatar/></div>
-            </section>
-            <section className={`flex w-full pt-10`}>
-                <Stats/>
-            </section>
+                <div className={`order-1 flex items-end justify-end md:w-2/5`}>
+                    <Avatar/>
+                </div>
+            </div>
         </section>
     </>
 }
