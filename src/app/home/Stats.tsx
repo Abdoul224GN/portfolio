cf99f44ca6cf14982c64a2faabf20dd5f510b91c
Abdoul Karim <karimdiallo625@gmail.com>
2025-05-01 11:20:18 +0000

    feat: Modification du font-size et de la taille des bordures

--- src/app/home/Stats.tsx
+++ src/app/home/Stats.tsx
@@ -29,7 +29,7 @@ export default function Stats() {
         <div className={`grid grid-cols-2 gap-2 md:flex md:flex-row justify-between w-full flex-col`}>
             {stats.map((item) => {
                 return <div className={`flex items-center justify-center md:justify-start gap-1 `}>
-                    <CountUp className={`text-4xl font-bold font-jetbrains`} end={item.num} key={item.id} duration={2}
+                    <CountUp className={`md:text-6xl text-4xl font-bold font-jetbrains`} end={item.num} key={item.id} duration={2}
                              delay={2}/>
                     <p className={`${item.text.length < 20 ? "max-w-[100px]" : "max-w-[150px]"}`}>{item.text}</p>
                 </div>
