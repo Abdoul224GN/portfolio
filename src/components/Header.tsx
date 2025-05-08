ec4f6b59c3832dba0e6f5f17111d47c9d7831e55
Abdoul Karim <karimdiallo625@gmail.com>
2025-05-08 16:34:25 +0000

    feat: Implementation du navMobile dans le Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,24 +3,31 @@ import {GithubIcon, LinkDin, MenuImg, TwitterIcon} from "../../public/Image"
 import Nav from "@/components/Nav";
 import {useState} from "react";
 import {Button} from "@/components/ui/button";
+import MobileNav from "@/components/MobileNav";
 
 export default function Header() {
     const [toggle, setToggle] = useState(false);
-    const linkStyle :string = "hover:opacity-75 transition-opacity";
+    const linkStyle: string = "hover:opacity-75 transition-opacity";
     console.log(toggle)
     return <>
-        <header className="h-8  flex justify-between items-center w-full mb-10">
-            <p className=" flex-1/3 text-2xl">Abdoul<span className="text-accent">.</span></p>
-            <Button className={`md:hidden size-9 p-0 pb-[1px]`} onClick={()=>setToggle(true)}><MenuImg className={``}/></Button>
-            <Nav/>
-            <div
-                className="h-full w-[125px] flex items-center justify-end pl-0.5 pr-0.5 pt-0 flex-1/3 max-md:hidden ">
-                <div
-                    className="rounded-full border-border border-2 bg-muted h-full w-[125px] flex justify-between items-center pl-0.5 pr-0.5 pt-0 ">
-                    <a href="#" className="h-full"><GithubIcon className={`${linkStyle}`}/></a>
-                    <a href="#" className="h-full"><LinkDin className={linkStyle}/></a>
-                    <a href="#" className="h-full"><TwitterIcon className={linkStyle} /></a>
+        <header className="h-8 flex flex-col justify-between items-center w-full mb-15">
+            <div className={`flex items-center justify-between w-full h-full`}>
+                <p className=" flex-1/3 text-2xl">Abdoul<span className="text-accent">.</span></p>
+                <Button className={`md:hidden size-9 p-0 pb-[1px]`} onClick={() => setToggle((prev) => !prev)}>
+                    <MenuImg/>
+                </Button>
+                <Nav/>
+                <div className="h-full w-[125px] flex items-center justify-end px-0.5 flex-1/3 max-md:hidden ">
+                    <div
+                        className="rounded-full border-border border-2 bg-muted h-full w-[125px] flex justify-between items-center px-0.5 ">
+                        <a href="#" className="h-full"><GithubIcon className={`${linkStyle}`}/></a>
+                        <a href="#" className="h-full"><LinkDin className={linkStyle}/></a>
+                        <a href="#" className="h-full"><TwitterIcon className={linkStyle}/></a>
+                    </div>
                 </div>
+            </div>
+            <div className={`pt-2 w-full flex items-center justify-center`}>
+                {toggle && <MobileNav/>}
             </div>
         </header>
     </>
--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -5,9 +5,9 @@ import {usePathname} from "next/navigation";
 export default function MobileNav() {
     const pathname = usePathname();
     return <>
-        <nav>
+        <nav className={`h-full border-2 border-accent rounded-full px-6 flex gap-3 md:hidden`}>
             {link.map((link, index) => (
-                <Link className={`${pathname === link.path && "text-accent underline decoration-dashed transition-all" } hover:text-accent
+                <Link className={`${pathname === link.path && "text-accent underline decoration-dashed transition-all text-[0.9rem]" } text-[0.9rem] hover:text-accent
                     `} href={link.path} key={index} >{link.name}</Link>
             ))}
         </nav>
