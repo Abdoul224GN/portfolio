fb65987bbcca1844d7b836728c218e637b465a6f
Abdoul Karim <karimdiallo625@gmail.com>
2025-05-10 11:39:46 +0000

    feat: Création d'un fichier pour les fonts

---
+++ src/app/font.tsx
@@ -0,0 +1,11 @@
+import {Exo_2, JetBrains_Mono} from "next/font/google";
+
+export const jetbrains = JetBrains_Mono({
+    subsets: ['latin'],
+    weight: ['700'],
+})
+
+export const barlow = Exo_2({
+    subsets : ['latin'],
+    weight: ['500']
+})
