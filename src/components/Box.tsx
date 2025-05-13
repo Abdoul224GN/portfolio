7b89163d8e274c4718ce291cd89caf2f486d713d
Abdoul Karim <karimdiallo625@gmail.com>
2025-05-13 18:08:34 +0000

    feat: Ajout de personalisation

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,5 +1,6 @@
 import {jetbrains} from "@/app/font";
 import {motion} from "framer-motion";
+import {ExternalLink} from "lucide-react"
 
 export default function Box({children, className}: { children?: React.ReactNode, className?: string }) {
     const project = [
@@ -54,11 +55,16 @@ export default function Box({children, className}: { children?: React.ReactNode,
         <section className={`px-30 grid grid-cols-3 gap-4`}>
             {project.map((project, index) => {
                 return <motion.div key={index}
-                                   className={`border-2 border-accent bg-muted rounded-2xl pointer p-5 ${className} cursor-pointer`}
-                                   whileHover={{translateY: -7.2}} transition={{duration: 0.1, ease: "easeInOut"}}>
-                <span
-                    className={`text-[2.5rem] font-bold text-outline text-transparent ${jetbrains.className}`}>{project.id}</span>
-                    <p className={`text-[20px] font-semibold mb-2`}>{project.title}</p>
+                                   className={`group border-2 border-accent bg-muted rounded-[10px] pointer p-5 ${className} cursor-pointer`}
+                                   whileHover={{translateY: -7.2, translateX:-7.2}} transition={{duration: 0.1, ease: "easeInOut"}}>
+                    <div className={` flex flex-row h-[45px] items-center justify-between hover:text-accent`}>
+                        <span
+                            className={`group-hover:text-outline-colored text-[2.5rem] font-bold text-outline text-transparent ${jetbrains.className}`}>
+                            {project.id}
+                        </span>
+                        <ExternalLink className={`rotate-90 hover:rotate-0 transition-all`}/>
+                    </div>
+                    <p className={`group-hover:text-accent text-[20px] font-semibold mb-2w transition-all`}>{project.title}</p>
                     <p className={`text-[15px]`}>{project.description}</p>
                 </motion.div>
             })}
