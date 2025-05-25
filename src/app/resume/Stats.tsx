0083d22c0465bbbf47dbbe97fec2236abf9f121b
Abdoul Karim <karimdiallo625@gmail.com>
2025-05-25 15:53:34 +0000

    feat: Ajout d'un suffixe pour les stats

--- src/app/resume/Stats.tsx
+++ src/app/resume/Stats.tsx
@@ -5,24 +5,27 @@ import CountUp from "react-countup";
 export default function Stats () {
     const stats = [
         {
-            name: "Google Review",
-            num : 220
+            name: "Satisfaction Rate",
+            num : 90,
+            suffix:"%"
         },
         {
             name: "Year Experience",
-            num : 15
+            num : 15,
+            suffix:"+"
         },
         {
-            name: "Google Review",
-            num : 220
+            name: "UI Designs",
+            num : 20,
+            suffix:"+"
         }
     ]
     return <>
-        <div className={`mt-7 grid grid-cols-3 gap-5 justify-center`}>
+        <div className={`mt-7 grid grid-cols-3 gap-3 justify-center`}>
             {stats.map((stat, index) => (
-                <div className={`shadow-md bg-muted h-full rounded-xl border border-white/20 p-4 flex flex-col items-center`} key={index}>
-                    <CountUp className={`font-semibold text-[20px]`} end={stat.num}/>
-                    <p className={`text-[12px]`}>{stat.name}</p>
+                <div className={`shadow-md bg-muted h-full rounded-xl border border-white/20 p-3 flex flex-col items-center`} key={index}>
+                    <CountUp className={`font-semibold text-[22px]`} end={stat.num} suffix={stat.suffix} />
+                    <p className={`text-[13px]`}>{stat.name}</p>
                 </div>
             ))}
         </div>
