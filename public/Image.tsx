9e1381a9e24aeaa5b414a60145c5bf23577dc86f
Abdoul Karim <karimdiallo625@gmail.com>
2025-05-26 18:41:45 +0000

    feat: Modification des Images

--- public/Image.tsx
+++ public/Image.tsx
@@ -42,6 +42,6 @@ export function MenuImg({className}:{className?:string}) {
 }
 export function Photo({src, className, }:{className?:string, src:string}) {
     return (
-        <Image src={src} width={250} height={250} priority={true} className={` h-full w-auto ${className}`} alt="menu" />
+        <Image src={src} width={300} height={300} priority={true} className={` h-auto w-auto ${className}`} alt="menu" />
     )
 }
--- src/app/resume/Skill.tsx
+++ src/app/resume/Skill.tsx
@@ -9,7 +9,7 @@ export default function Skill() {
             title: "UI/UX Designer",
             name: "Satisfaction Rate",
             description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi aperiam, cupiditate deserunt dolores eligendi esse expedita explicabo fugiat inte maiores modi molestias mollitia nihil nisi placeat suscipit voluptatum?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum error exercitationem explicabo, fuga ipsam nisi possimussapiente. Consectetur dignissimos esse nesciunt pferspiciatis tempora veritatis vitae! Delectus deleniti excepturi maxime veniam",
-            imageURL: "/images/photo1.webp",
+            imageURL: "/images/DESIGN.webp",
             stats : [
                 {
                     name: "Satisfaction Rate",
@@ -32,7 +32,7 @@ export default function Skill() {
             title: "FullStack Developer",
             name: "Year Experience",
             description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi aperiam, cupiditate deserunt dolores eligendi esse expedita explicabo fugiat inte maiores modi molestias mollitia nihil nisi placeat suscipit voluptatum?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum error exercitationem explicabo, fuga ipsam nisi possimussapiente. Consectetur dignissimos esse nesciunt pferspiciatis tempora veritatis vitae! Delectus deleniti excepturi maxime veniam",
-            imageURL: "/images/photo2.webp",
+            imageURL: "/images/Engineering.png",
             stats : [
                 {
                     name: "Satisfaction Rate",
@@ -55,7 +55,7 @@ export default function Skill() {
             title: "Software Engineering",
             name: "UI Designs",
             description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi aperiam, cupiditate deserunt dolores eligendi esse expedita explicabo fugiat inte maiores modi molestias mollitia nihil nisi placeat suscipit voluptatum?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum error exercitationem explicabo, fuga ipsam nisi possimussapiente. Consectetur dignissimos esse nesciunt pferspiciatis tempora veritatis vitae! Delectus deleniti excepturi maxime veniam",
-            imageURL: "/images/photo2.webp",
+            imageURL: "/images/SEO.webp",
             stats : [
                 {
                     name: "Satisfaction Rate",
@@ -78,14 +78,14 @@ export default function Skill() {
     return <>
         <title>Resume</title>
         {skills.map((skill, index) => (
-            <div className={`grid max-md:grid-cols-1 grid-cols-2 gap-x-10 h-auto mt-20 max-md:shadow-md max-md:bg-muted max-md:shadow-black/15 max-md:rounded-[10px] p-5 `} key={index}>
+            <div className={`grid max-md:grid-cols-1 grid-cols-2 gap-x-13 h-auto mt-20 max-md:shadow-md max-md:bg-muted max-md:shadow-black/15 max-md:rounded-[10px] p-5 `} key={index}>
                 <div className={`group max-md:hidden relative ${index % 2 === 0 ? "order-2" : "order-1"}`}>
                     <div
-                        className={`group-hover:left-65 group-hover:-top-25 transition-all absolute top-65 -left-25 w-30 h-30 translate-14 bg-accent rounded-full `}>
+                        className={`group-hover:left-65 group-hover:-top-25 transition-all absolute top-60  -left-25 w-25 h-25 translate-14 bg-accent rounded-full `}>
                     </div>
                     <div
-                        className={`backdrop-blur-md shadow-md border-white/20 border flex items-center justify-center bg-white/10 rounded-2xl h-[400px]`}>
-                        <Photo src={skill.imageURL}></Photo>
+                        className={`backdrop-blur-md shadow-md border-white/20 border flex items-center justify-center bg-white/10 rounded-2xl h-[371px]`}>
+                        <Photo src={skill.imageURL} className={`rounded-2xl`}></Photo>
                     </div>
                 </div>
                 <div className={`h-full ${index % 2 === 0 ? "order-1" : "order-2"}`}>
