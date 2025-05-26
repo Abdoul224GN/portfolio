edabdb546d8cc09fffb02eed8794546442853754
Abdoul Karim <karimdiallo625@gmail.com>
2025-05-26 09:41:25 +0000

    feat: Configuration pour l'optimisation du bundle JS

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
 };
-
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+})
+module.exports = withBundleAnalyzer({})
 export default nextConfig;
