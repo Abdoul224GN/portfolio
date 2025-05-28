1faa991eb3b70e03312f1736cf7c07f5940dd882
Abdoul Karim <karimdiallo625@gmail.com>
2025-05-28 15:03:18 +0000

    feat: Mise en place de la responsive

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,16 +7,16 @@ import {Button} from "@/components/ui/button";
 export default function Contact() {
     return <>
         <Header/>
-        <section className={`mx-30`}>
-            <div className={`shadow-md bg-muted grid grid-cols-[2fr_3fr] gap-5 p-10 rounded-2xl w-full`}>
+        <section className={`max-lg:mx-5 mx-30`}>
+            <div className={`shadow-md bg-muted grid max-md:grid-cols-1 grid-cols-[2fr_3fr] gap-5 max-md:p-5 p-10 rounded-2xl w-full`}>
                 <div className={``}>
-                    <div className={`backdrop-blur-2xl bg-white/10 border shadow-md border-white/20 rounded-2xl`}>
+                    <div className={`max-md:hidden backdrop-blur-2xl bg-white/10 border shadow-md border-white/20 rounded-2xl`}>
                         <ContactPhoto className={`h-full w-full rounded-2xl`}/>
                     </div>
                 </div>
                 <div className={`w-full`}>
                     <form className={`flex flex-col gap-y-5`}>
-                        <div className={`grid grid-cols-2 gap-x-4`}>
+                        <div className={`grid grid-cols-2 gap-y-4 max-sm:grid-cols-1 gap-x-4`}>
                             <div className={``}>
                                 <Label>Your name</Label>
                                 <Input className={`w-full`} placeholder={`Enter your name`}/>
