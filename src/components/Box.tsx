7885ec0f701c78b5f7c8a7304b27c00d5721932b
Abdoul Karim <karimdiallo625@gmail.com>
2025-05-30 16:37:13 +0000

    feat: : Separation des donnés de "Projects"

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -4,73 +4,9 @@ import {ExternalLink} from "lucide-react"
 import {useState} from "react";
 import {Button} from "@/components/ui/button";
 import FadeInOnScroll from "@/components/FadeInOnScroll";
+import {projects} from "@/components/data/projects";
 
 export default function Box({children, className}: { children?: React.ReactNode, className?: string }) {
-    const projects = [
-        {
-            id: 1,
-            title: 'Web Developement',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad architecto debitis delectus',
-            link: "#",
-            technologies: ["Java", "JavaScript", "CSS"],
-        },
-        {
-            id: 2,
-            title: 'Web Developement',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad architecto debitis delectus',
-            link: "#",
-            technologies: ["TypeScript", "TailwindCSS"],
-        },
-        {
-            id: 3,
-            title: 'Web Developement',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad architecto debitis delectus',
-            link: "#",
-            technologies: ["Spring Boot", "ReactJS"]
-        },
-        {
-            id: 4,
-            title: 'Web Developement',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad architect debitis delectus',
-            link: "#",
-            technologies: ["Java", "Python"]
-        },
-        {
-            id: 5,
-            title: 'Web Developement',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad architecto debitis delectus',
-            link: "#",
-            technologies: ["Java", "Python"]
-        },
-        {
-            id: 6,
-            title: 'Web Developement',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad architecto debitis delectus',
-            link: "#",
-            technologies: ["Java", "Python"]
-        },
-        {
-            id: 7,
-            title: 'Web Developement',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad architecto debitis delectus',
-            link: "#",
-            technologies: ["Java", "Python"]
-        },
-        {
-            id: 8,
-            title: 'Web Developement',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad architecto debitis delectus',
-            link: "#",
-            technologies: ["Java", "Python"]
-        },
-        {
-            id: 9,
-            title: 'Web Developement',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad architecto debitis delectus',
-            link: "#",
-            technologies: ["Java", "Python"]
-        }
-    ]
     const [showMore, setShowMore] = useState(true);
     const GRID_LIMIT = 6;
     const firstSix = projects.slice(0, GRID_LIMIT);
---
+++ src/components/data/projects.js
@@ -0,0 +1,65 @@
+export const projects = [
+    {
+        id: 1,
+        title: 'Web Developement',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad architecto debitis delectus',
+        link: "#",
+        technologies: ["Java", "JavaScript", "CSS"],
+    },
+    {
+        id: 2,
+        title: 'Web Developement',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad architecto debitis delectus',
+        link: "#",
+        technologies: ["TypeScript", "TailwindCSS"],
+    },
+    {
+        id: 3,
+        title: 'Web Developement',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad architecto debitis delectus',
+        link: "#",
+        technologies: ["Spring Boot", "ReactJS"]
+    },
+    {
+        id: 4,
+        title: 'Web Developement',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad architect debitis delectus',
+        link: "#",
+        technologies: ["Java", "Python"]
+    },
+    {
+        id: 5,
+        title: 'Web Developement',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad architecto debitis delectus',
+        link: "#",
+        technologies: ["Java", "Python"]
+    },
+    {
+        id: 6,
+        title: 'Web Developement',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad architecto debitis delectus',
+        link: "#",
+        technologies: ["Java", "Python"]
+    },
+    {
+        id: 7,
+        title: 'Web Developement',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad architecto debitis delectus',
+        link: "#",
+        technologies: ["Java", "Python"]
+    },
+    {
+        id: 8,
+        title: 'Web Developement',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad architecto debitis delectus',
+        link: "#",
+        technologies: ["Java", "Python"]
+    },
+    {
+        id: 9,
+        title: 'Web Developement',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad architecto debitis delectus',
+        link: "#",
+        technologies: ["Java", "Python"]
+    }
+]
