04cdd3397a494ac62ba9938b79545ba11bb0f937
Abdoul Karim <karimdiallo625@gmail.com>
2025-06-19 14:31:41 +0000

    feat: Arrangement du Nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -8,10 +8,10 @@ import {link} from "@/components/navLink";
 export default function () {
     const pathname = usePathname();
     return <>
-        <nav className="container h-full shadow shadow-black/30 bg-muted w-1/2 rounded-full pl-15 pr-15 flex justify-between items-center max-md:hidden">
+        <nav className="flex flex-row gap-5 container h-full shadow shadow-black/30 bg-muted w-auto rounded-full pl-15 pr-15 flex justify-between items-center max-md:hidden">
             {link.map((link, index) => {
                 return (
-                    <Link className={`${pathname === link.path && "text-accent underline decoration-dashed transition-all" } hover:text-accent
+                    <Link className={`${pathname === link.path && "text-accent underline decoration-dashed transition-all" } w-auto whitespace-nowrap  hover:text-accent
                     `} href={link.path} key={index} >{link.name}</Link>
                 )
             })}
