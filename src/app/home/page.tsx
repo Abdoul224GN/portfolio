41e7bb97c35d602e1ecf449e4fea43c67c35ab77
Abdoul Karim <karimdiallo625@gmail.com>
2025-06-19 14:22:07 +0000

    feat: Ajout d'une animation d'apparition et de disparition du Header lors du scroll;

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -9,7 +9,7 @@ import {JavaCode, NextjsCode} from "@/components/Badge";
 
 export default function Home() {
     return <>
-        <section className={` flex flex-col justify-items-center h-full`}>
+        <section className={`mt-10 flex flex-col justify-items-center h-full`}>
 
             <div className={`flex flex-col md:flex-row items-center`}>
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import Nav from "@/components/Nav";
 import {useState} from "react";
 import {Button} from "@/components/ui/button";
 import MobileNav from "@/components/MobileNav";
-import {motion} from "framer-motion";
+import {motion, useMotionValueEvent, useScroll} from "framer-motion";
 
 export default function Header() {
     const [toggle, setToggle] = useState(false);
@@ -14,8 +14,28 @@ export default function Header() {
         visible: {y: 0, opacity: 1},
         hidden: {y: -100, opacity: 0},
     }
+    const {scrollY} = useScroll();
+    const [hidden, setHidden] = useState(false);
+
+    useMotionValueEvent(scrollY, 'change', (latest) => {
+        const previous = scrollY.getPrevious();
+
+        // @ts-ignore
+        if (latest > previous && latest > 150) {
+            setHidden(true); // scroll vers le bas → on cache le header
+        } else {
+            setHidden(false); // scroll vers le haut → on affiche
+        }
+    });
     return <>
-        <header className="h-8 flex flex-col justify-between items-center w-full mb-5 max-md:mb-10">
+        <motion.header
+            className="fixed border-y mt-0.5 border-white/20 z-50 top-0 left-0 bg-background h-10 flex flex-col justify-between items-center w-full p-1  mb-5 max-md:mb-10"
+            variants={{
+                visible: {y: 0},
+                hidden: {y: '-100%'},
+            }}
+            animate={hidden ? 'hidden' : 'visible'}
+            transition={{duration: 0.35, ease: 'easeInOut'}}>
             <div className={`flex items-center justify-between w-full h-full`}>
                 <p className=" flex-1/3 text-2xl">Abdoul<span className="text-accent">.</span></p>
                 <Button className={`md:hidden size-9 p-0 pb-[1px]`} onClick={() => setToggle((prev) => !prev)}>
@@ -31,10 +51,10 @@ export default function Header() {
                     </div>
                 </div>
             </div>
-            <motion.div animate={toggle ? boxVariants.visible : boxVariants.hidden} transition={{type:"spring"}}
+            <motion.div animate={toggle ? boxVariants.visible : boxVariants.hidden} transition={{type: "spring"}}
                         className={`pt-2 w-full flex items-center justify-center`}>
                 <MobileNav/>
             </motion.div>
-        </header>
+        </motion.header>
     </>
 }
