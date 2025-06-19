a382bb755de8c28f31876383e7db3c16bb64f240
Abdoul Karim <abdoul625karim@gmail.com>
2025-06-19 15:44:23 +0000

    feat: Suppression des élément inutilisé

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -8,12 +8,13 @@ import {link} from "@/components/navLink";
 export default function () {
     const pathname = usePathname();
     return <>
-        <nav className="flex flex-row gap-5 container h-full shadow shadow-black/30 bg-muted w-auto rounded-full pl-15 pr-15 flex justify-between items-center max-md:hidden">
+        <nav
+            className="flex flex-row gap-5 container h-full shadow shadow-black/30 bg-muted w-auto rounded-full pl-15 pr-15 flex justify-between items-center max-md:hidden">
             {link.map((link, index) => {
                 return (
                     <Link duration={500} offset={-30} smooth={true} spy={true} activeClass={`text-accent`}
-                        className={`active:text-accent w-auto whitespace-nowrap  hover:text-accent
-                    `} href={link.path} key={index} to={link.path} >{link.name}</Link>
+                          className={`active:text-accent w-auto whitespace-nowrap  hover:text-accent
+                    `} key={index} to={link.path}>{link.name}</Link>
                 )
             })}
         </nav>
